c2010378fad559122709a596d63181fbf86cdcd6
Artur Paikin <artur@arturpaikin.com>
2022-04-19 01:40:52 +0100

    Just warn on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.svg',
   organizationName: 'facebook', // Usually your GitHub org/user name.
