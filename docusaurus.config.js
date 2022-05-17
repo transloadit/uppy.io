1f772fd465d663ee92016e81efde8cb60d4f995f
Murderlon <merlijn@soverin.net>
2022-05-17 15:15:08 +0200

    Upgrade to latest docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      autoCollapseSidebarCategories: true,
+      docs: { sidebar: { autoCollapseCategories: true }},
       navbar: {
         title: 'Uppy',
         logo: {
