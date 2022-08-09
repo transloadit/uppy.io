0f9e149b61682ae41579712e346cc98698970556
Murderlon <merlijn@soverin.net>
2022-08-09 14:48:28 +0200

    Theme tweaks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,124 +1,124 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/nightOwlLight')
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark')
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Uppy',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo.svg',
-  organizationName: 'transloadit', // Usually your GitHub org/user name.
-  projectName: 'uppy.io', // Usually your repo name.
+	title: 'Uppy',
+	tagline: 'Dinosaurs are cool',
+	url: 'https://your-docusaurus-test-site.com',
+	baseUrl: '/',
+	onBrokenLinks: 'warn',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/logo.svg',
+	organizationName: 'transloadit', // Usually your GitHub org/user name.
+	projectName: 'uppy.io', // Usually your repo name.
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          breadcrumbs: false,
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/transloadit/uppy.io/tree/main',
-        },
-        blog: {
-          showReadingTime: true,
-          editUrl: 'https://github.com/transloadit/uppy.io/tree/main/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					breadcrumbs: false,
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/transloadit/uppy.io/tree/main',
+				},
+				blog: {
+					showReadingTime: true,
+					editUrl: 'https://github.com/transloadit/uppy.io/tree/main/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: { sidebar: { autoCollapseCategories: true } },
-      colorMode: { disableSwitch: true },
-      navbar: {
-        title: 'Uppy',
-        logo: {
-          alt: 'Uppy Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'quick-start',
-            position: 'left',
-            label: 'Docs',
-          },
-          { to: '/examples', label: 'Examples', position: 'left' },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/transloadit/uppy',
-            label: 'GitHub',
-            position: 'left',
-          },
-          {
-            href: 'https://community.transloadit.com/',
-            label: 'Community Support Forum',
-            position: 'left',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/quick-start',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-}
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			docs: { sidebar: { autoCollapseCategories: true } },
+			colorMode: { disableSwitch: true },
+			navbar: {
+				title: 'Uppy',
+				logo: {
+					alt: 'Uppy Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'quick-start',
+						position: 'left',
+						label: 'Docs',
+					},
+					{ to: '/examples', label: 'Examples', position: 'left' },
+					{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/transloadit/uppy',
+						label: 'GitHub',
+						position: 'left',
+					},
+					{
+						href: 'https://community.transloadit.com/',
+						label: 'Community Support Forum',
+						position: 'left',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Tutorial',
+								to: '/docs/quick-start',
+							},
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Stack Overflow',
+								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+							},
+							{
+								label: 'Discord',
+								href: 'https://discordapp.com/invite/docusaurus',
+							},
+							{
+								label: 'Twitter',
+								href: 'https://twitter.com/docusaurus',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/facebook/docusaurus',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+};
 
-module.exports = config
+module.exports = config;
