99f9bd95a38e0ad15d86f1963da539d2089605e8
Murderlon <merlijn@soverin.net>
2023-01-10 16:17:49 +0100

    Format

--- src/components/QuickStartLinks.tsx
+++ src/components/QuickStartLinks.tsx
@@ -14,18 +14,16 @@ export const QuickStartLinks = (props: Props) => {
 	return (
 		<div className="not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-2">
 			{props.items.map((item) => (
-				<div className="group relative rounded-xl border border-solid border-[color:var(--ifm-color-emphasis-300)] hover:border-uppy transition-colors">
+				<div className="group relative rounded-xl border border-solid border-[color:var(--ifm-color-emphasis-300)] transition-colors hover:border-uppy">
 					<div className="absolute -inset-px rounded-xl"></div>
 					<div className="relative overflow-hidden rounded-xl p-6">
-						<h2 className="mt-4 font-display text-lg text-slate-900 ">
+						<h2 className="font-display mt-4 text-lg text-slate-900 ">
 							<Link to={item.link} className="hover:no-underline">
 								<span className="absolute -inset-px rounded-xl"></span>
 								{item.name}
 							</Link>
 						</h2>
-						<p>
-							{item.description}
-						</p>
+						<p>{item.description}</p>
 					</div>
 				</div>
 			))}
