6d420429c658fb4118feef044bbefad53f6e4ce7
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-01-10 15:56:16 +0100

    meta: add support for TS in lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2022,
+		jsx: false,
 	},
 	env: { node: false },
 	plugins: ['@docusaurus', 'markdown', 'mdx'],
-	extends: ['plugin:@docusaurus/recommended'],
+	extends: [
+		'plugin:@docusaurus/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
 	overrides: [
 		{
 			files: ['./*.js'],
@@ -32,7 +36,26 @@ module.exports = {
 			},
 		},
 		{
-			files: ['**/*.md/*.jsx', '**/*.mdx/*.jsx'],
+			files: [
+				'**/*.ts',
+				'**/*.tsx',
+				'**/*.md/*.ts',
+				'**/*.mdx/*.ts',
+				'**/*.md/*.tsx',
+				'**/*.mdx/*.tsx',
+			],
+			parser: '@typescript-eslint/parser',
+			plugins: ['@typescript-eslint'],
+		},
+		{
+			files: [
+				'**/*.jsx',
+				'**/*.tsx',
+				'**/*.md/*.jsx',
+				'**/*.mdx/*.jsx',
+				'**/*.md/*.tsx',
+				'**/*.mdx/*.tsx',
+			],
 			parserOptions: {
 				ecmaFeatures: {
 					jsx: true,
