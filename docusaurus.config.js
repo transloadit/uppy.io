a7e18df5921819e5e5662f24f3d78c549eb06dc3
Murderlon <merlijn@soverin.net>
2023-03-22 11:22:06 +0100

    fixup! Add GitHub Action for deploying (#79)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
 	title: 'Uppy',
 	tagline: 'Sleek, modular open source JavaScript file uploader',
 	url: 'https://uppy.io',
-	baseUrl: '/',
+	baseUrl: '/uppy.io/',
 	onBrokenLinks: 'warn',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/logo.svg',
