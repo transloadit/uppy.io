f330575678c1a391412d07c4ab992d71ff30f35a
Murderlon <merlijn@soverin.net>
2023-11-01 14:57:28 +0100

    Upgrade to Docusaurus V3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const { themes } = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -16,6 +16,9 @@ const config = {
 	organizationName: 'transloadit', // Usually your GitHub org/user name.
 	projectName: 'uppy.io', // Usually your repo name.
 	trailingSlash: true,
+	markdown: {
+		format: 'detect',
+	},
 	presets: [
 		[
 			'classic',
@@ -154,7 +157,6 @@ const config = {
 			},
 			prism: {
 				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
 			},
 		}),
 };
