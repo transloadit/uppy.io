aa0bc4e7dcf7d24cc8da52ac007dbdce7b9a3f2f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-04-10 00:13:59 +0200

    meta: add custom plugin to handle symlinks
    Webpack has a special option for handling symbolink links.

---
+++ src/plugins/custom.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (context, options) {
+	return {
+		name: 'custom-docusaurus-plugin',
+		configureWebpack(config, isServer, utils) {
+			return {
+				resolve: {
+					symlinks: false,
+				},
+			};
+		},
+	};
+};
