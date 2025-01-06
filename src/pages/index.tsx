f3cba497a8514d5b13b3e7b3a055d343a5a6d5ae
Mikael Finstad <finstaden@gmail.com>
2025-01-06 22:08:59 +0800

    enable facebook (#300)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -264,6 +264,7 @@ export default function Home(): JSX.Element {
 												// 'GoogleDrive',
 												'Dropbox',
 												// 'Instagram',
+												'Facebook',
 												'Url',
 												'OneDrive',
 												'Unsplash',
