c390e5a0f21d31c529ce1ff2663537bb62c06d21
Prakash <qxprakash@gmail.com>
2025-12-04 03:51:44 +0530

    fix dashboard example bug (#406)

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -289,6 +289,11 @@ const Uppy = ({ state, locale }) => {
 						'Images and video only, 2–3 files, up to 1 MB'
 					:	null
 				}
+				metaFields={
+					state.restrictions ?
+						[{ id: 'caption', name: 'Caption', placeholder: 'Add a Caption' }]
+					:	undefined
+				}
 			/>
 		</div>
 	);
